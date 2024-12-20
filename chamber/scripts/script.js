f20a53a2665e90baf1e9bd47a959b7a8711275ee
JosemaRodas <114843092+JosemaRodas@users.noreply.github.com>
2024-12-19 20:26:45 -0400

    commit

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -66,8 +66,3 @@ function displayForecast(forecastData) {
 
 getWeather();
 
-document.addEventListener('DOMContentLoaded', function() {
-    const timestampField = document.getElementById('timestamp');
-    timestampField.value = new Date().toISOString();
-
-    const modals = document.querySelectorAll
